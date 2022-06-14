fbadd8c8074c0351647affb673728f9d35fd5d10
chinmaylahoti <chinmay.lahoti@gmail.com>
2022-06-14 15:42:29 +0530

    first commit

---
+++ Sentiment Analysis - R.R
@@ -0,0 +1,79 @@
+Step 1
+
+library(tidytext)
+Sentiments
+
+Step 2
+
+get_sentiments("bing")
+
+library(janeaustenr)
+library(stringr)
+library(tidytext)
+
+Step 3
+
+tidy_data <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+Step 4
+
+positive_senti <- get_sentiments("bing") %>%
+  filter(sentiment == "positive")
+
+tidy_data %>%
+  filter(book == "Emma") %>%
+  semi_join(positive_senti) %>%
+  count(word, sort = TRUE)
+
+Step 5
+
+library(tidyr)
+bing <- get_sentiments("bing")
+Emma_sentiment <- tidy_data %>%
+  inner_join(bing) %>%
+  count(book = "Emma" , index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+Step 6
+
+library(ggplot2)
+
+ggplot(Emma_sentiment, aes(index, sentiment, fill = book)) +
+  geom_bar(stat = "identity", show.legend = TRUE) +
+  facet_wrap(~book, ncol = 2, scales = "free_x")
+
+Step 7
+
+counting_words <- tidy_data %>%
+  inner_join(bing) %>%
+  count(word, sentiment, sort = TRUE)
+head(counting_words)
+
+Step 8
+
+counting_words %>%
+  filter(n > 150) %>%
+  mutate(n = ifelse(sentiment == "negative", -n, n)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = sentiment))+
+  geom_col() +
+  coord_flip() +
+  labs(y = "Sentiment Score")
+
+Step 9
+
+library(reshape2)
+library(wordcloud)
+tidy_data %>%
+  inner_join(bing) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("red", "dark green"),
+                   max.words = 100)
